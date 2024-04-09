750509d683450ace3a934f60e7895f4670a3ae3d
beliy016 <beliy1@ro.ru>
2024-04-09 18:10:59 +0300

    add new project

---
+++ src/main/java/com/example/kata_git/KataGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.kata_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kata_git/KataGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kata_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
